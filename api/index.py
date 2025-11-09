39633616715108d9c6c8b5200728b325e53250cd
David francisco López Casallas <lopezcasallasdavidfrancisco@gmail.com>
2025-11-08 23:41:22 -0500

    preuba vercel dos

--- api/index.py
+++ api/index.py
@@ -1,5 +1,7 @@
 from app import app
 
-# Para Vercel
+# Esto es necesario para que Vercel ejecute la aplicación correctamente
+application = app
+
 if __name__ == '__main__':
     app.run(debug=False)
